95cfe4d54802c7a287ae44e6302dbb3012f3c290
MurathanKurfali <murathankurfali@gmail.com>
2025-02-10 00:41:42 +0100

    metric fix

--- tasks/exeter/utils.py
+++ tasks/exeter/utils.py
@@ -4,21 +4,6 @@ import numpy as np
 from sklearn.metrics import f1_score
 import logging
 
-def f1_subclaim(predictions, references):  # This is a passthrough function
-    _prediction = predictions[0]
-    _reference = references[0]
-    print(f"_prediction: {_prediction}")
-    print(f"_reference: {_reference}")
-    string_label = ["0_0", "1_1", "1_2", "1_3", "1_4", "1_6", "1_7", "2_1", "2_3", "3_1", "3_2", "3_3", "4_1", "4_2", "4_4", "4_5", "5_1", "5_2"]
-
-    reference = string_label.index(_reference)
-    prediction = (
-        string_label.index(_prediction)
-        if _prediction in string_label
-        else not bool(reference)
-    )
-
-    return (prediction, reference)
 
 def f1(predictions, references):  # This is a passthrough function
 
