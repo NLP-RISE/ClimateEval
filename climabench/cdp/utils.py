1f78820f47ddc84303ee57357f4423053d427989
MurathanKurfali <murathankurfali@gmail.com>
2025-02-11 16:12:02 +0100

    cdp topic update

--- climabench/cdp/utils.py
+++ climabench/cdp/utils.py
@@ -2,21 +2,6 @@ import datasets
 import numpy as np
 import sklearn.metrics
 
-labels = ["Adaptation", "Buildings", "Climate Hazards", "Emissions", "Energy", "Food", "Governance and Data Management", "Opportunities", "Strategy", "Transport", "Waste", "Water"]
-columns_to_drop = ["id", "Year Reported to CDP", "Organization", "Parent Section", "Section",
-                   "Question Name", "Row Name", "Comments", "Response Answer"]
-
-def label_to_id(example):
-    """Maps the Category/Label to an index based on the labels list."""
-    example["label_index"] = labels.index(example["Label"])
-    return example
-
-
-def prepare_topic_dataset(dataset: datasets.Dataset) -> datasets.Dataset:
-
-    dataset = dataset.remove_columns(columns_to_drop)
-    dataset = dataset.map(label_to_id)  # Add label index column
-    return dataset
 
 def f1(predictions, references):  # This is a passthrough function
 
