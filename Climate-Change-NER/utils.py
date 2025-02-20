003e1c178d3a38157c01deadbbc58bcfc24d11e0
MurathanKurfali <murathankurfali@gmail.com>
2025-02-20 16:44:31 +0100

    ner utils update

--- Climate-Change-NER/utils.py
+++ Climate-Change-NER/utils.py
@@ -1,26 +1,59 @@
-import datasets
+from pprint import pprint
+
 import numpy as np
 import sklearn.metrics
 
-def f1(predictions, references):  # This is a passthrough function
 
-    _prediction = predictions[0]
-    _reference = references[0]
-    print(f"_prediction: {_prediction}")
-    print(f"_reference: {_reference}")
-    string_label = ['irrelevant', 'relevant']
-    reference = string_label.index(_reference)
-    prediction = (
-        string_label.index(_prediction)
-        if _prediction in string_label
-        else not bool(reference)
-    )
+def entity_f1(predictions, references):
+    """
+    Computes precision, recall, and F1-score for NER task.
 
-    return (prediction, reference)
+    :param predictions: List of dictionaries with entity types as keys and lists of entities as values.
+    :param references: List of dictionaries with gold-standard entity labels.
+    :return: (precision, recall, f1-score) tuple per instance.
+    """
 
-def agg_f1(items):
+    precision_scores = []
+    recall_scores = []
+    f1_scores = []
+    print("Prediction:")
+    pprint(predictions)
+    print("Gold:")
 
-    references, predictions = zip(*items)
-    references, predictions = np.asarray(references), np.asarray(predictions)
+    pprint(references)
 
-    return sklearn.metrics.f1_score(references, predictions, average='macro')
+    for pred, ref in zip(predictions, references):
+        pred_entities = set((etype, ent) for etype, ents in pred.items() for ent in ents)
+        ref_entities = set((etype, ent) for etype, ents in ref.items() for ent in ents)
+
+        true_positives = len(pred_entities & ref_entities)
+        false_positives = len(pred_entities - ref_entities)
+        false_negatives = len(ref_entities - pred_entities)
+
+        precision = true_positives / (true_positives + false_positives + 1e-9)
+        recall = true_positives / (true_positives + false_negatives + 1e-9)
+        f1 = 2 * (precision * recall) / (precision + recall + 1e-9)
+
+        precision_scores.append(precision)
+        recall_scores.append(recall)
+        f1_scores.append(f1)
+
+    return list(zip(precision_scores, recall_scores, f1_scores))
+
+
+def agg_entity_f1(items):
+    """
+    Aggregates F1-score across dataset instances.
+
+    :param items: List of tuples containing precision, recall, and F1-score for each instance.
+    :return: Mean F1-score across instances.
+    """
+
+    precision, recall, f1_scores = zip(*items)
+    precision, recall, f1_scores = np.asarray(precision), np.asarray(recall), np.asarray(f1_scores)
+
+    return {
+        "precision": np.mean(precision),
+        "recall": np.mean(recall),
+        "f1": np.mean(f1_scores)
+    }
